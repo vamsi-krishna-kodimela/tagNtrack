ab265f27dd75f51c6cbdc8caa4831d73e6a65439
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2023-04-12 23:53:03 +0530

    Added place holder files for each folder

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { mongooseConnect } from "./config/db.config";
+import Routes from "./routes/routes";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(Routes);
 
 mongooseConnect(() => {
   app.listen(PORT, () => {
