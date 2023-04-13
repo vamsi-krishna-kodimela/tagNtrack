0e722957349835b10c0caf9f38fa626b384d3877
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2023-04-14 00:29:49 +0530

    Added get shipments routes

--- src/controllers/shipment.controller.ts
+++ src/controllers/shipment.controller.ts
@@ -5,6 +5,7 @@ import Shipment from "../schema/shipment.schema";
 import ShipmentStatus from "../schema/shipment-status.schema";
 import DeliveryDetails from "../schema/delivery-details.schema";
 import UserType from "../interfaces/user-type.enum";
+import { type } from "os";
 
 const createShipment = async (
   req: PopulatedRequest<IShipment>,
@@ -79,4 +80,65 @@ const getShipmentsById = async (req: Request, res: Response) => {
   }
 };
 
-export { createShipment, getShipmentsById };
+const getShipments = async (req: PopulatedRequest<any>, res: Response) => {
+  const user = req.user;
+  if (user?.type == UserType[UserType.Customer]) {
+    const shipments = await Shipment.find({ bookedBy: user })
+      .populate({
+        path: "bookedBy",
+        strictPopulate: false,
+        select: "-password",
+      })
+      .populate({
+        path: "deliveryPartner",
+        strictPopulate: false,
+        select: "-password",
+      })
+      .populate({
+        path: "PickupDetails",
+        strictPopulate: false,
+      })
+      .populate({
+        path: "deliveryDetails",
+        strictPopulate: false,
+      })
+      .populate({
+        path: "latestStatus",
+        strictPopulate: false,
+      });
+    if (shipments) {
+      res.json(shipments);
+      return;
+    } else {
+      res.json([]);
+      return;
+    }
+  } else {
+    const shipments = await Shipment.find()
+      .populate({
+        path: "bookedBy",
+        strictPopulate: false,
+        select: "-password",
+      })
+      .populate({
+        path: "deliveryPartner",
+        strictPopulate: false,
+        select: "-password",
+      })
+      .populate({
+        path: "PickupDetails",
+        strictPopulate: false,
+      })
+      .populate({
+        path: "deliveryDetails",
+        strictPopulate: false,
+      })
+      .populate({
+        path: "latestStatus",
+        strictPopulate: false,
+      });
+    res.json(shipments);
+  }
+};
+
+export { createShipment, getShipmentsById, getShipments };
--- src/routes/shipment.routes.ts
+++ src/routes/shipment.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   createShipment,
+  getShipments,
   getShipmentsById,
 } from "../controllers/shipment.controller";
 
 const router = Router();
 
 router.route("/create").post(createShipment);
+router.route("/").get(getShipments);
 router.route("/:id").get(getShipmentsById);
 
 export = router;
